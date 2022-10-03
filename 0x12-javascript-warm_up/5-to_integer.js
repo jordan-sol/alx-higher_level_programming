71fb08bd1112b7173d62aa40c816f8004d88e5a3
jordan-sol <101702526+jordan-sol@users.noreply.github.com>
2022-10-03 14:47:08 +0300

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-// prints two arguments passed to it, in the following format: “ is ”
-
-if (isNaN(process.argv[2])) {
+/* script that prints My number: <first argument converted in integer>
+if the first argument can be converted to an integer */
+const argv = parseInt(process.argv[2]);
+if (Number.isNaN(argv)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + argv);
 }
